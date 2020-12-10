8eaacdf23938b251ba57e197860044ad811b7de7
gustavoluchi <gustavo.luchi@gmail.com>
2020-12-10 19:58:04 -0300

    exercício 2

--- exercício-2/app.js
+++ exercício-2/app.js
@@ -1,1 +1,5 @@
-let arrayDeCidades = [];
+//Crie uma função que receba um uma lista de números e imprima o quadrado de cada número.
+const arrayAoQuadrado = (...array) => array.map((a) => a**2);
+
+const resultado = arrayAoQuadrado(2, 3, 6, 7);
+console.log('Resultado: ', resultado);
