6dee5d53a610b85ce3c62fea77e7132e399706e7
gustavoluchi <gustavo.luchi@gmail.com>
2020-12-11 19:13:50 -0300

    exercício 9

---
+++ exercício-9/app.js
@@ -0,0 +1,5 @@
+// Declare uma arrowFunction que deverá possuir dois parâmetros (nome, idade). A nossa função deve mostrar no console a seguinte frase: “Olá, eu sou <nome>, e tenho <idade> anos”.
+
+const ola = (nome, idade) => `Olá, eu sou ${nome} e tenho ${idade} anos`;
+
+console.log(ola('gustavo', 26));
