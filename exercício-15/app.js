073467011c0459dfad290365dd581add5af140f8
gustavoluchi <gustavo.luchi@gmail.com>
2020-12-12 00:34:45 -0300

    exercício 15 v2

--- exercício-15/app.js
+++ exercício-15/app.js
@@ -1,7 +1,6 @@
 let cidadesJSON = require('./Cidades.json');
 // 23 = RS, conforme Estados.json
-let resultado = cidadesJSON.filter((a, b, c) => {
-    //console.log(a);
-    return a.Estado === '23'});
+let resultado = cidadesJSON.filter((itemAtual) => {
+    return itemAtual.Estado === '23'});
 
 console.log(resultado);
